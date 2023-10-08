0517e985fe3a1b3f2505b1dcd2034b5205cd00e0
liaisontw <liaison.tw@gmail.com>
2023-10-08 22:19:53 +0800

    Initial commit

---
+++ app/src/main/java/io/liaison/tasks/TasksFragment.kt
@@ -0,0 +1,29 @@
+package io.liaison.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.liaison.tasks.databinding.FragmentTasksBinding
+
+class TasksFragment : Fragment() {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
