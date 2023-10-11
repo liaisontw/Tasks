1227f27f6843c84a506ba11db4f8c4ebee35244a
liaisontw <liaison.tw@gmail.com>
2023-10-11 12:35:48 +0800

    Recycler view with data binding

--- app/src/main/java/io/liaison/tasks/TasksFragment.kt
+++ app/src/main/java/io/liaison/tasks/TasksFragment.kt
@@ -34,7 +34,8 @@ class TasksFragment : Fragment() {
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?. let {
-                adapter.data = it
+                //adapter.data = it
+                adapter.submitList(it)
             }
         })
 
